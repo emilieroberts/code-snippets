a8a91ca161659384e6ad4843be9fcc9ffb8b2c5a
hadrosaur <hadrosaur@google.com>
2018-03-08 17:53:40 -0800

    Added Tab key navigation snippet

--- keyboard_nextfocus.java
+++ keyboard_nextfocus.java
@@ -1,4 +1,1 @@
-myView.setNextFocusLeftId(R.id.view_to_left);
-myView.setNextFocusRightId(R.id.view_to_right);
-myView.setNextFocusTopId(R.id.view_to_top);
-myView.setNextFocusBottomId(R.id.view_to_bottom);
+myView.setNextFocusForwardId(R.id.next_view);
